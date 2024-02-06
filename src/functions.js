453c55a3c0138fef0bb36df5b4fefc947c05fce1
Bob Hodges <bobhodges1@outlook.com>
2024-02-06 11:15:54 +0000

    finished level 150 - made episode data a function to be reused and added missing info from episode page

--- src/functions.js
+++ src/functions.js
@@ -1,7 +1,16 @@
+const { gameOfThronesEpisodes } = require("./data/gameOfThronesData");
+
 function createPaddedEpisode(season, episode) {
     const seasonPaddedNumber = season < 10 ? `0${season}` : `${season}`;
     const episodePaddedNumber = episode < 10 ? `0${episode}` : `${episode}`;
     return "S" + seasonPaddedNumber + "E" + episodePaddedNumber;
 }
 
-module.exports = { createPaddedEpisode };
+function findEpisodeId(episodeId) {
+    const episodeData = gameOfThronesEpisodes.find(
+        (episode) => episode.id == episodeId,
+    );
+    return episodeData;
+}
+
+module.exports = { createPaddedEpisode, findEpisodeId };
